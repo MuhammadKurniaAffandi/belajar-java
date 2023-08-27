82acc027feadf0c6fafabf6b55368fff01cd80c2
Muhammad Kurnia Affandi <affandi.kurnia@gmail.com>
2023-08-27 10:49:49 +0700

    Edit gitignore

--- belajar-java-dasar-01/src/TipeDataChar.java
+++ belajar-java-dasar-01/src/TipeDataChar.java
@@ -1,6 +1,10 @@
 public class TipeDataChar {
     public static void main(String[] args) {
         char m = 'M';
+        char k = 'K';
+        char a = 'A';
         System.out.println(m);
+        System.out.println(k);
+        System.out.println(a);
     }
 }
