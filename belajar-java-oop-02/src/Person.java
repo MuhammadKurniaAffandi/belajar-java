50646cd71e12385a7d7bfca616d9118b720d1a99
Muhammad Kurnia Affandi <affandi.kurnia@gmail.com>
2023-09-07 17:06:12 +0700

    Mengenal Method, Constructor dan Constructor Overloading

--- belajar-java-oop-02/src/Person.java
+++ belajar-java-oop-02/src/Person.java
@@ -2,4 +2,29 @@ class Person {
     String name;
     String address;
     final String country = "Indonesia";
+
+    // Constructor class Person
+    Person(String paramName, String paramAddress){
+        name = paramName;
+        address = paramAddress;
+    }
+
+    // Constructor Overloading
+    Person(String paramName){
+        // name = paramName;
+
+        /* cara memanggil Constructor lain*/
+        this(paramName, null);
+
+    }
+
+    // Constructor Overloading
+    Person(){
+        /* cara memanggil Constructor lain*/
+        this(null);
+
+    }
+    void sayHello(String paramName) {
+        System.out.println("Hello " + paramName + ", My name is " + name);
+    }
 }
--- belajar-java-oop-02/src/PersonApp.java
+++ belajar-java-oop-02/src/PersonApp.java
@@ -1,15 +1,22 @@
 public class PersonApp {
     public static void main(String[] args) {
-        /* Beberapa cara untuk membuat Object*/
-        var person1 = new Person();
-        person1.name = "Affandi";
-        person1.address = "Jakarta";
-//        Person person2 = new Person();
-//        Person person3;
-//        person3 = new Person();
+        /* cara untuk membuat Object*/
+        // var person1 = new Person();
+        // person1.name = "Affandi";
+        // person1.address = "Jakarta";
 
+        // Menggunakan Constructor
+        var person1 = new Person("Muhammad", "Jakarta");
         System.out.println(person1.name);
         System.out.println(person1.address);
         System.out.println(person1.country);
+        person1.sayHello("Pepen");
+
+        // Menggunakan Constructor Overloading
+        Person person2 = new Person("Kurnia");
+        Person person3;
+        person3 =  new Person();
+        person3.name = "Fandi";
+        person3.sayHello("Budi");
     }
 }
